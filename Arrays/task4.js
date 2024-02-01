a583da58baa8f3b59d86b03b4d950e4b05664673
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-02-01 17:08:56 +0530

    Number of Good Pairs

---
+++ Arrays/task4.js
@@ -0,0 +1,37 @@
+// Task4: Number of Good Pairs
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+// Input: nums = [1,1,1,1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+// ************************************************
+
+// var numIdenticalPairs = function (nums) {
+//   let goodPairs = [];
+
+//   for (let i = 0; i <= nums.length; i++) {
+//     for (let j = i + 1; j <= nums.length; j++) {
+//       if (nums[i] === nums[j]) {
+//         goodPairs.push([i, j]);
+//       }
+//     }
+//   }
+//   return goodPairs.length;
+// };
+
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(numIdenticalPairs([1, 2, 3]));
