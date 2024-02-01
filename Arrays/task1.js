8944eba1a370bd23b20a52ad839c024b493cf666
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-02-01 15:50:37 +0530

    Concatenation of Array

--- Arrays/task1.js
+++ Arrays/task1.js
@@ -1,1 +1,25 @@
-// Task1:
+// Task1: Concatenation of Array
+
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+// Example 2:
+
+// Input: nums = [1,3,2,1]
+// Output: [1,3,2,1,1,3,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+// - ans = [1,3,2,1,1,3,2,1]
+
+// ********************************************
+
+var getConcatenation = function (nums) {
+  // let repnums = [...nums]
+  // return nums.concat(repnums);
+
+  return [...nums, ...nums];
+};
+
+console.log(getConcatenation([1, 2, 1]));
