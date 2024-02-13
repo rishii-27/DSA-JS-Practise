2af1964baf2f3bfda97b8d348573aac8e4919e3d
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-02-13 14:56:19 +0530

    Sorting using BubbleSort, InsertionSort, SelectionSort

---
+++ Searching and Sorting/task2.js
@@ -0,0 +1,66 @@
+// Task2: Sorting using BubbleSort, InsertionSort, SelectionSort
+
+// ***********************************************
+
+function bblSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+var arr = [234, 43, 55, 63, 5, 6, 235, 547];
+console.log("Bubble Sort Array:", bblSort(arr));
+
+// ---------------------------------------------
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentValue;
+  }
+  return arr;
+}
+console.log("Insertion Sort Array:", insertionSort([2, 1, 3, 7, 5]));
+
+// -------------------------------------------
+
+function selectionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    // Find the index of the minimum element in the unsorted part of the array
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the found minimum element with the first element
+    if (minIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
+
+// Example usage:
+const unsortedArray = [64, 25, 12, 22, 11];
+const sortedArray = selectionSort(unsortedArray); // Create a copy to keep the original array unmodified
+
+console.log("Selection Sort Array:", sortedArray);
