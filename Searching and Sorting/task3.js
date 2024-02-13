8662962179ce073fe26be52d07e812932fc2938e
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-02-13 15:08:33 +0530

    Find Intersection of Arrays

---
+++ Searching and Sorting/task3.js
@@ -0,0 +1,36 @@
+// Task3: Find Intersection of Arrays
+
+// ***********************************************
+// Function to find the intersection of two arrays
+function findIntersection(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  const temp = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      i++;
+    } else if (arr2[j] < arr1[i]) {
+      j++;
+    } else {
+      temp.push(arr1[i]);
+      i++;
+      j++;
+    }
+  }
+
+  return temp;
+}
+
+const arr1 = [1, 5, 10, 20, 40, 80];
+const arr2 = [6, 7, 20, 80, 100];
+const arr3 = [3, 4, 15, 20, 30, 70, 120];
+
+// Find the intersection of arr1 and arr2
+const temp = findIntersection(arr1, arr2);
+console.log("Common elements present in all arr1 and arr2 are: ", temp);
+
+// Find the intersection of temp and arr3 to get common elements
+const ans = findIntersection(temp, arr3);
+
+console.log("Common elements present in all 3 arrays are:", ans);
