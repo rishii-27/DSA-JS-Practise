30be74199a6745a3832aaa6431293ae4f0b74681
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-02-28 18:54:52 +0530

    Linked List cycle

---
+++ Linked List/task2.js
@@ -0,0 +1,42 @@
+// Task2: Linked List Cycle 
+// https://leetcode.com/problems/linked-list-cycle/description/
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+// Input: head = [1,2], pos = 0
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+// **************************************************
+
+
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// Example usage:
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+// Create the linked list with cycle
+let head = new ListNode(3);
+head.next = new ListNode(2);
+head.next.next = new ListNode(0);
+head.next.next.next = new ListNode(-4);
+head.next.next.next.next = head.next; // Creating cycle
+
+console.log(hasCycle(head));
